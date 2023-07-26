e311b45c86c326c748aa2263905ebb550a7a43b6
aydan <aydan@i-on.net>
2023-07-26 13:53:12 +0900

    이미지 삭제 기능 추가

--- src/component/common/forms/DefaultFile.tsx
+++ src/component/common/forms/DefaultFile.tsx
@@ -86,6 +86,11 @@ const DefaultFile = React.forwardRef(
         });
     };
 
+    const deleteImageHandler = (src: string) => {
+      setParam(param.filter((val) => val !== src));
+      console.log(src);
+    };
+
     useEffect(() => {
       if (defaultValue) {
         setParam(defaultValue.split(","));
@@ -97,33 +102,58 @@ const DefaultFile = React.forwardRef(
     }, [param, setValue]);
 
     return (
-      <>
-        <input
-          className={
-            className === ""
-              ? "text-sm block w-full h-7 text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-              : className
-          }
-          type="file"
-          accept="image/*"
-          placeholder={placeholder}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            changeParamHandler(event)
-          }
-          multiple={multiple}
-          disabled={disable}
-          ref={ref}
-        />
+      <div>
+        <label className="relative inline-flex items-center cursor-pointer">
+          <div className="mr-2 text-white bg-violet-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            파일첨부
+          </div>
+          <input
+            // className={
+            //   className === ""
+            //     ? "text-sm block w-full h-7 text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            //     : className
+            // }
+            className="sr-only"
+            type="file"
+            accept="image/*"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              changeParamHandler(event)
+            }
+            multiple={multiple}
+            disabled={disable}
+            ref={ref}
+          />
+        </label>
         <div>
           {param && param.length > 0 ? (
             param.map((src: string, index: number) => (
-              <img key={index} src={src} />
+              <div
+                className="bg-gray-400 flex justify-center relative mt-3"
+                key={index}
+              >
+                <img className="object-cover h-max" src={src} />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="absolute w-16 h-16 right-0 z-10"
+                  onClick={() => deleteImageHandler(src)}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </div>
             ))
           ) : (
             <></>
           )}
         </div>
-      </>
+      </div>
     );
   }
 );
