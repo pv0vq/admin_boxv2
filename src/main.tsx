a30ec06658c05cd0d461d073cd62388df43ddc2f
pv0vq <tig00566@naver.com>
2023-08-01 14:55:56 +0900

    수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./Router";
+import router from "./Router.js";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
